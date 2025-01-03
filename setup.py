fb917c8408f69937aa453091e5d4920ea04356aa
dhjs0000 <3110197220@qq.com>
2025-01-03 21:39:34 +0800

    delete 多余的package选项

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/dhjs0000/PyPlus",
-    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
